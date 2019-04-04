f5659381a7e5d1e2e636a40f35b18d13ba1bafb4
Boris Stoyanov <boris.stoyanov@chaosgroup.com>
2019-04-04 15:55:07 +0300

    static camera

--- draw/camera.go
+++ draw/camera.go
@@ -1,9 +1,31 @@
 package draw
 
-import "github.com/templarrei/golearnyouatracer/geom"
+import (
+	"math"
+
+	"github.com/templarrei/golearnyouatracer/geom"
+)
 
 type Camera struct {
 	LLCorner, Horizontal, Vertical, Origin geom.Vec
+
+	theta, halfW, halfH float64
+}
+
+func NewCamera(vFOV, aspectRatio float64) Camera {
+	theta := vFOV * math.Pi / 180
+	halfH := math.Tan(theta / 2)
+	halfW := aspectRatio * halfH
+
+	return Camera{
+		LLCorner:   geom.NewVec(-halfW, -halfH, -1),
+		Horizontal: geom.NewVec(halfW*2, 0, 0),
+		Vertical:   geom.NewVec(0, halfH*2, 0),
+		Origin:     geom.NewVec(0, 0, 0),
+		theta:      theta,
+		halfW:      halfW,
+		halfH:      halfH,
+	}
 }
 
 func (c Camera) Ray(u, v float64) geom.Ray {
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 
 	"github.com/templarrei/golearnyouatracer/draw"
@@ -16,19 +17,19 @@ func main() {
 	const x, y float64 = 200, 100
 
 	scene := draw.NewScene(x, y)
-	cam := draw.Camera{
-		LLCorner:   geom.NewVec(-2, -1, -1),
-		Horizontal: geom.NewVec(4, 0, 0),
-		Vertical:   geom.NewVec(0, 2, 0),
-		Origin:     geom.NewVec(0, 0, 0),
-	}
+	cam := draw.NewCamera(90, x/y)
+	// l := draw.NewList(
+	//     geom.NewSphere(geom.NewVec(0, 0, -1), 0.5, &material.Lambertian{Albedo: geom.NewVec(0.1, 0.2, 0.5)}),
+	//     geom.NewSphere(geom.NewVec(0, -100.5, -1), 100, &material.Lambertian{Albedo: geom.NewVec(0.8, 0.8, 0)}),
+	//     geom.NewSphere(geom.NewVec(1, 0, -1), 0.5, material.NewMetal(geom.NewVec(0.8, 0.6, 0.2), 0.3)),
+	//     geom.NewSphere(geom.NewVec(-1, 0, -1), 0.5, material.NewGlass(1.5)),
+	//     geom.NewSphere(geom.NewVec(-1, 0, -1), -0.45, material.NewGlass(1.5)),
+	// )
 
+	r := math.Cos(math.Pi / 4)
 	l := draw.NewList(
-		geom.NewSphere(geom.NewVec(0, 0, -1), 0.5, &material.Lambertian{Albedo: geom.NewVec(0.1, 0.2, 0.5)}),
-		geom.NewSphere(geom.NewVec(0, -100.5, -1), 100, &material.Lambertian{Albedo: geom.NewVec(0.8, 0.8, 0)}),
-		geom.NewSphere(geom.NewVec(1, 0, -1), 0.5, material.NewMetal(geom.NewVec(0.8, 0.6, 0.2), 0.3)),
-		geom.NewSphere(geom.NewVec(-1, 0, -1), 0.5, material.NewGlass(1.5)),
-		geom.NewSphere(geom.NewVec(-1, 0, -1), -0.45, material.NewGlass(1.5)),
+		geom.NewSphere(geom.NewVec(-r, 0, -1), r, &material.Lambertian{Albedo: geom.NewVec(0, 0, 1)}),
+		geom.NewSphere(geom.NewVec(r, 0, -1), r, &material.Lambertian{Albedo: geom.NewVec(1, 0, 0)}),
 	)
 
 	fmt.Println(scene.WritePPM(f, l, 100, cam))
