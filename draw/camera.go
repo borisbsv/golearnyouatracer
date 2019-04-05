c7e483875bf2dfa1193ff1826a14c038bcd489f2
Boris Stoyanov <boris.stoyanov@chaosgroup.com>
2019-04-05 11:23:28 +0300

    chapter 11

--- draw/camera.go
+++ draw/camera.go
@@ -2,15 +2,21 @@ package draw
 
 import (
 	"math"
+	"math/rand"
+	"time"
 
 	"github.com/templarrei/golearnyouatracer/geom"
 )
 
 type Camera struct {
 	LLCorner, Horizontal, Vertical, Origin geom.Vec
+	u, v, w                                geom.Vec
+	lensRadius                             float64
 }
 
-func NewCamera(lFrom, lAt, vUp geom.Vec, vFOV, aspectRatio float64) Camera {
+var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func NewCamera(lFrom, lAt, vUp geom.Vec, vFOV, aspectRatio, focusDist, aperture float64) Camera {
 	theta := vFOV * math.Pi / 180
 	halfH := math.Tan(theta / 2)
 	halfW := aspectRatio * halfH
@@ -19,25 +25,50 @@ func NewCamera(lFrom, lAt, vUp geom.Vec, vFOV, aspectRatio float64) Camera {
 	u := vUp.Cross(w).ToUnit()
 	v := w.Cross(u)
 
+	x := u.Scale(halfW * focusDist)
+	y := v.Scale(halfH * focusDist)
+	z := w.Scale(focusDist)
+
 	llCorner := lFrom.
-		Sub(u.Scale(halfW)).
-		Sub(v.Scale(halfH)).
-		Sub(w)
+		Sub(x).
+		Sub(y).
+		Sub(z)
+
 	return Camera{
 		Origin:     lFrom,
 		LLCorner:   llCorner,
-		Horizontal: u.Scale(2 * halfW),
-		Vertical:   v.Scale(2 * halfH),
+		Horizontal: x.Scale(2),
+		Vertical:   y.Scale(2),
+		lensRadius: aperture / 2,
+
+		u: u,
+		v: v,
 	}
 }
 
 func (c Camera) Ray(s, t float64) geom.Ray {
+	rd := randomInUnitDisc().Scale(c.lensRadius)
+	offset := c.u.
+		Scale(rd.X()).
+		Add(c.v.Scale(rd.Y()))
+
 	return geom.NewRay(
-		c.Origin,
+		c.Origin.Add(offset),
 		c.LLCorner.
 			Add(c.Horizontal.Scale(s)).
 			Add(c.Vertical.Scale(t)).
 			Sub(c.Origin).
+			Sub(offset).
 			ToUnit(),
 	)
+}
+
+func randomInUnitDisc() geom.Vec {
+	var p geom.Vec
+	for {
+		p = geom.NewVec(rng.Float64(), rng.Float64(), 0).Scale(2).Sub(geom.NewVec(1, 1, 0))
+		if p.Dot(p) < 1 {
+			return p
+		}
+	}
 }
--- main.go
+++ main.go
@@ -16,12 +16,17 @@ func main() {
 	const x, y float64 = 200, 100
 
 	scene := draw.NewScene(x, y)
+	lFrom := geom.NewVec(3, 3, 2)
+	lAt := geom.NewVec(0, 0, -1)
+	vUp := geom.NewVec(0, 1, 0)
 	cam := draw.NewCamera(
-		geom.NewVec(-2, 2, 1),
-		geom.NewVec(0, 0, -1),
-		geom.NewVec(0, 1, 0),
-		30,
+		lFrom,
+		lAt,
+		vUp,
+		20,
 		x/y,
+		lFrom.Sub(lAt).Len(),
+		1,
 	)
 	l := draw.NewList(
 		geom.NewSphere(geom.NewVec(0, 0, -1), 0.5, &material.Lambertian{Albedo: geom.NewVec(0.1, 0.2, 0.5)}),
@@ -30,12 +35,6 @@ func main() {
 		geom.NewSphere(geom.NewVec(-1, 0, -1), 0.5, material.NewGlass(1.5)),
 		geom.NewSphere(geom.NewVec(-1, 0, -1), -0.45, material.NewGlass(1.5)),
 	)
-
-	// r := math.Cos(math.Pi / 4)
-	// l := draw.NewList(
-	//     geom.NewSphere(geom.NewVec(-r, 0, -1), r, &material.Lambertian{Albedo: geom.NewVec(0, 0, 1)}),
-	//     geom.NewSphere(geom.NewVec(r, 0, -1), r, &material.Lambertian{Albedo: geom.NewVec(1, 0, 0)}),
-	// )
 
 	fmt.Println(scene.WritePPM(f, l, 100, cam))
 }
